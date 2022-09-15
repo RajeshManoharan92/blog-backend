f6b7a89cf1714a157d6daf1e7239fb3d97e509e9
RajeshManoharan92 <m.rajeshmanohar@gmail.com>
2022-09-15 23:18:30 +0530

    backend

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,4 @@
-const User = require("../model/User");
+const User = require("../model/User"); 
 const bcrypt = require("bcryptjs");
 var nodemailer = require('nodemailer');
 var randomString = require('random-string')
